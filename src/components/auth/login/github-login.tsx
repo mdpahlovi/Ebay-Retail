c9f06cd07fae18050f1acb8a381acd60a538a280
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-12-22 21:02:52 +0600

    [Done] Google Login

--- src/components/auth/login/github-login.tsx
+++ src/components/auth/login/github-login.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Github } from "lucide-react";
+import { toast } from "react-toastify";
 
 export default function GithubLogin({ navigateFrom }: { navigateFrom: () => void }) {
     return (
-        <Button variant="outline">
+        <Button variant="outline" onClick={() => toast.error("Currently not available")}>
             <Github size={16} className="mr-2" />
             Github
         </Button>
